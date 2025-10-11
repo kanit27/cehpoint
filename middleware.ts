c072193a2859652ab9fdc549f0092b87dba35b0e
Kanit Gohel <kanitgohel27@gmail.com>
2025-10-12 02:57:54 +0530

    final another commit

--- middleware.ts
+++ middleware.ts
@@ -18,11 +18,11 @@ export function middleware(request: NextRequest) {
   }
 
   // If the user is logged in (has a token) and tries to access signin/signup, redirect them to home
-  if (['/signin', '/signup'].includes(pathname) && token) {
-    const url = request.nextUrl.clone();
-    url.pathname = '/home';
-    return NextResponse.redirect(url);
-  }
+  // if (['/signin', '/signup'].includes(pathname) && token) {
+  //   const url = request.nextUrl.clone();
+  //   url.pathname = '/home';
+  //   return NextResponse.redirect(url);
+  // }
 
   // Allow the request to proceed
   return NextResponse.next();
